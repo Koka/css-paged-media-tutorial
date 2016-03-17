98340c739694565ddb281f1f83a74ab05ff964d1
Andreas Jung <yet@gmx.de>
2016-03-17 10:10:51 +0100

    updated

--- docs/fix_titles.py
+++ docs/fix_titles.py
@@ -18,6 +18,22 @@ for name in os.listdir(html_dir):
         nodes[0].text = title
         print 'changed'
         print fname
+
+
+    nodes = root.xpath('//head')
+    if nodes:
+        head = nodes[0]
+        node = lxml.etree.Element('meta')
+        node.attrib['name'] = 'description'
+        node.attrib['content'] = 'CSS Paged Media (PDF generation from XML and HTML using CSS stylesheets) tutorial and showcase with lessons, tool descriptions and comparions. PDFreactor Antennahouse PrinceXML'
+        head.append(node)
+
+        node = lxml.etree.Element('meta')
+        node.attrib['name'] = 'keywords'
+        node.attrib['content'] = 'CSS,PDF,HTML,XML, CSS Paged media, Antennahouse, PDFreactor, PrinceXML, Markup' 
+        head.append(node)
+
+
     html = lxml.html.tostring(root)
     with open(fname, 'wb') as fp:
         fp.write(html.encode('utf8'))
