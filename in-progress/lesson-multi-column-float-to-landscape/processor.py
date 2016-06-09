037ef1e70358253eeecb0693ec96084928ed0069
Andreas Jung <yet@gmx.de>
2016-06-08 19:50:17 -0700

    updated

--- in-progress/lesson-multi-column-float-to-landscape/processor.py
+++ in-progress/lesson-multi-column-float-to-landscape/processor.py
@@ -3,7 +3,9 @@ import sys
 import tempfile
 import lxml.html
 import shutil
+import pprint
 import commands
+import PyPDF2
 from lxml.cssselect import CSSSelector
 
 
@@ -29,6 +31,7 @@ class Processor(object):
         self.index2_html = os.path.join(self.tmpdir, 'index2.html')
         self.index2_pdf= os.path.join(self.tmpdir, 'index2.pdf')
         self.index2_areatree = os.path.join(self.tmpdir, 'index2.areatree')
+        self.pdf_final = os.path.join(self.tmpdir, 'final.pdf')
         shutil.copy(input_filename, self.index_html)
         shutil.copytree('styles', os.path.join(self.tmpdir, 'styles'))
         self._log('copied {} -> {}'.format(self.input_filename, self.index_html))
@@ -117,6 +120,66 @@ class Processor(object):
         if status != 0:
             raise RuntimeError('{} executed with status {}'.format(cmd, status))
 
+    def process_floatables(self):
+
+
+        with open(self.index2_areatree, 'rb') as fp:
+            root = lxml.etree.fromstring(fp.read())
+
+        result = dict()
+        for node in root.xpath('//*[contains(@text,"id=")]'):
+            current = node
+            while current.tag != '{http://www.antennahouse.com/names/XSL/AreaTree}PageViewportArea':
+                current = current.getparent()
+
+            page_no = int(current.attrib['page-number'])
+            abs_page_no = int(current.attrib['abs-page-number'])
+            text = node.attrib['text']
+            
+            d = dict(
+                    text=text,
+                    page_no=page_no,
+                    abs_page_no=abs_page_no)
+            for item in text.split('::'):
+                k, v = item.split('=')
+                d[k] = v
+            result[abs_page_no] = d
+
+        self._log(pprint.pformat(result))
+
+        with open(self.index2_pdf, 'rb') as fp_in:
+            reader = PyPDF2.PdfFileReader(fp_in)
+            writer = PyPDF2.PdfFileWriter()
+
+            for page_no in range(reader.numPages):
+                page_data = result.get(page_no + 1)
+                page = reader.getPage(page_no)
+                if page_data:
+                    floatable_id = page_data['id']
+                    floatable_html_fn = os.path.join(self.tmpdir, '{}.html'.format(floatable_id))
+                    floatable_html_fn2 = os.path.join(self.tmpdir, '{}-2.html'.format(floatable_id))
+                    # wrap floatable HTML snippet with template.html
+                    with open(os.path.join(self.tmpdir, 'template.html'), 'rb') as template_in:
+                        with open(floatable_html_fn, 'rb') as floatable_html_in:
+                            template_html = template_in.read()
+                            template_html = template_html.format(body=floatable_html_in.read())
+                            with open(floatable_html_fn2, 'wb') as floatable_html_out:
+                                floatable_html_out.write(template_html)
+
+                    floatable_pdf_fn = os.path.join(self.tmpdir, '{}.pdf'.format(floatable_id))
+                    self.run_ah(floatable_html_fn, floatable_pdf_fn)
+                    with open(floatable_pdf_fn, 'rb') as floatable_in:
+                        print '*'*80
+                        print 'merging', floatable_pdf_fn
+                        floatable_reader = PyPDF2.PdfFileReader(floatable_in)
+                        page.mergePage(floatable_reader.getPage(0))
+
+                writer.addPage(page)
+
+            with open(self.pdf_final, 'wb') as fp_out:
+                writer.write(fp_out)
+                print 'resulting PDF: {}'.format(self.pdf_final)
+
     def __str__(self):
         return '{}(logfile={}, workdir={})'.format(self.__class__.__name__, self.logfile, self.tmpdir)
 
@@ -125,10 +188,8 @@ class Processor(object):
         self.extract_flowables()
         self.create_pdf()
         self.create_pdf(areatree=True)
-
+        self.process_floatables()
 
 if __name__ == '__main__':
     proc = Processor('index.html')
-    import pdb; pdb.set_trace() 
-    print proc
     proc()
